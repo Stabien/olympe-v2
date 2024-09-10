6615f6fe30cabd254310ca7d83d6ddcd24cb1100
Stabien <bastien.piedallu@gmail.com>
2024-09-11 00:24:03 +0200

    feat(back): add docker dev config

---
+++ back/src/app.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeOrmConfig } from './config/typeorm.config';
+import { UserModule } from './user/user.module';
+import { UserModule } from './user/user.module';
+
+@Module({
+  imports: [ConfigModule.forRoot(), TypeOrmModule.forRoot(typeOrmConfig()), UserModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
--- src/lib/components/base/AppCard.svelte
+++
@@ -1,7 +0,0 @@
-<script lang="ts">
-	import { Card } from 'flowbite-svelte';
-</script>
-
-<Card class="app-shadow">
-	<slot />
-</Card>
