e7f40b6b30b11dc4c1a0df368f165abdb9542ab9
Stabien <bastien.piedallu@gmail.com>
2024-10-11 20:27:26 +0200

    refactor(back): remove exercise service to put back logic in controller

--- back/src/exercises/exercises.controller.ts
+++ back/src/exercises/exercises.controller.ts
@@ -2,14 +2,29 @@ import { Controller, Post, Body, Patch, Param, Delete } from '@nestjs/common'
 import { ExercisesService } from './exercises.service'
 import { CreateExerciseDto } from './dto/create-exercise.dto'
 import { UpdateExerciseDto } from './dto/update-exercise.dto'
+import { ProgramsService } from 'src/programs/programs.service'
 
 @Controller('exercises')
 export class ExercisesController {
-  constructor(private readonly exercisesService: ExercisesService) {}
+  constructor(
+    private readonly exercisesService: ExercisesService,
+    private readonly programsService: ProgramsService,
+  ) {}
 
   @Post()
   async create(@Body() createExerciseDto: CreateExerciseDto) {
     return await this.exercisesService.create(createExerciseDto)
+  }
+
+  @Post('/programs/:programId')
+  async createAndAssignToProgram(
+    @Param('programId') programId: string,
+    @Body() createExerciseDto: CreateExerciseDto,
+  ) {
+    const exercise = await this.exercisesService.create(createExerciseDto)
+    await this.programsService.addExercise(programId, exercise.id)
+
+    return exercise
   }
 
   @Patch(':id')
