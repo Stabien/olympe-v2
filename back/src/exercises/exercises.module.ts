9ea66673a360ae238c23d4c8af2422baf6277380
Stabien <bastien.piedallu@gmail.com>
2024-10-11 20:27:26 +0200

    refactor(back): remove exercise service to put back logic in controller

--- back/src/exercises/exercises.module.ts
+++ back/src/exercises/exercises.module.ts
@@ -3,9 +3,10 @@ import { ExercisesService } from './exercises.service'
 import { ExercisesController } from './exercises.controller'
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { Exercise } from './entities/exercise.entity'
+import { ProgramsModule } from 'src/programs/programs.module'
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Exercise])],
+  imports: [TypeOrmModule.forFeature([Exercise]), ProgramsModule],
   controllers: [ExercisesController],
   providers: [ExercisesService],
 })
