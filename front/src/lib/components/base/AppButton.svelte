df395ec81c8be9e7e8da1953f9e3d7d00fd9e5dc
Stabien <bastien.piedallu@gmail.com>
2024-11-14 02:07:55 +0100

    feat(front): add loading on button

--- front/src/lib/components/base/AppButton.svelte
+++ front/src/lib/components/base/AppButton.svelte
@@ -1,7 +1,17 @@
 <script lang="ts">
-	import { Button } from 'flowbite-svelte'
+	import { Button, Spinner } from 'flowbite-svelte'
+
+	export let isLoading: boolean = false
 </script>
 
-<Button {...$$restProps} class="duration-200 {$$restProps.class}" on:click>
-	<slot />
+<Button
+	{...$$restProps}
+	class="h-12 duration-200 {$$restProps.class} ring-0 focus-within:ring-0"
+	on:click
+>
+	{#if isLoading}
+		<Spinner size={6} color="white" />
+	{:else}
+		<slot />
+	{/if}
 </Button>
