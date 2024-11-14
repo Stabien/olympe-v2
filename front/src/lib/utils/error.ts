07b85ebaeeafe0ae19f21f6f1e44bb07cecf8700
Stabien <bastien.piedallu@gmail.com>
2024-11-14 02:06:31 +0100

    feat(front): add error message system

--- front/src/lib/utils/error.ts
+++ front/src/lib/utils/error.ts
@@ -1,3 +1,11 @@
-export const handleApiError = (error: Response) => {
-  const { status } = error
+export const defaultErrorMessage = 'Une erreur est survenue'
+
+export const httpErrorMessages: Record<string, Record<number, string>> = {
+	login: {
+		404: 'Email ou mot de passe incorrect'
+	}
+}
+
+export const getHttpErrorMessage = (scope: string, statusCode: number) => {
+	return httpErrorMessages[scope][statusCode] ?? defaultErrorMessage
 }
